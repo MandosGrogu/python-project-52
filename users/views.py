b35d1302984d239b04b3f731ee37f8e05d525dbe
MandosGrogu <jpinkmann@mail.ru>
2026-09-25 21:05:39 +0300

    add fullname field

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
+from django.db.models import Value
+from django.db.models.functions import Concat
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
 
 def index(request):
-    users = get_user_model().objects.all()
+    users = get_user_model().objects.annotate(
+    fullname=Concat('first_name', Value(' '), 'last_name')
+)
     return render(
         request,
         "users.html",
